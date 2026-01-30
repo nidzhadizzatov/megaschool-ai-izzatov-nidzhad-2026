9214242443b359e5780eab8771e7a7f14e34dfab
Ниджад Иззатов <izzatov.nidzhad@mail.ru>
2026-01-30 18:04:19 +0300

    fix: resolve issue #9
    Array index out of bounds in calculateSum() - JavaScript

--- demo/calculator.js
+++ demo/calculator.js
@@ -10,7 +10,7 @@
  */
 function calculateSum(numbers) {
     let sum = 0;
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         sum += numbers[i];
     }
     return sum;

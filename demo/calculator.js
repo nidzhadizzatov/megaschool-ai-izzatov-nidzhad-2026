75a91f20bd84e02a3e49a4bf6c51345e55231110
Ниджад Иззатов <izzatov.nidzhad@mail.ru>
2026-01-30 18:01:15 +0300

    feat: add JavaScript calculator module

---
+++ demo/calculator.js
@@ -0,0 +1,48 @@
+/**
+ * Simple calculator utility
+ * @module calculator
+ */
+
+/**
+ * Calculate sum of array elements
+ * @param {number[]} numbers - Array of numbers to sum
+ * @returns {number} Sum of all numbers
+ */
+function calculateSum(numbers) {
+    let sum = 0;
+    for (let i = 0; i <= numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+/**
+ * Calculate average of array elements
+ * @param {number[]} numbers - Array of numbers
+ * @returns {number} Average value
+ */
+function calculateAverage(numbers) {
+    const sum = calculateSum(numbers);
+    return sum / numbers.length;
+}
+
+/**
+ * Find maximum value in array
+ * @param {number[]} numbers - Array of numbers
+ * @returns {number} Maximum value
+ */
+function findMax(numbers) {
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+module.exports = {
+    calculateSum,
+    calculateAverage,
+    findMax
+};
